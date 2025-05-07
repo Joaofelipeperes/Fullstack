963bb3078f999433568f8147b9b4bfd810d6018d
Philippe Augusto <66576938+Philippe-Augusto@users.noreply.github.com>
2025-05-07 15:32:08 -0300

    Update Calendario.java

--- src/main/java/com/fullstack/projeto/Calendario.java
+++ src/main/java/com/fullstack/projeto/Calendario.java
@@ -20,8 +20,68 @@ public class Calendario {
     private LocalDate dataFim;
 
     @Column(name = "ano_semestre")
-    private LocalDate anoSemestre;
+    private String anoSemestre;
 
     @Enumerated(EnumType.STRING)
     private Tipos tipo;
+
+    // Construtores
+    public Calendario() {}
+
+    public Calendario(String nome, LocalDate dataInicio, LocalDate dataFim, String anoSemestre, Tipos tipo) {
+        this.nome = nome;
+        this.dataInicio = dataInicio;
+        this.dataFim = dataFim;
+        this.anoSemestre = anoSemestre;
+        this.tipo = tipo;
+    }
+
+    // Getters e Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public LocalDate getDataInicio() {
+        return dataInicio;
+    }
+
+    public void setDataInicio(LocalDate dataInicio) {
+        this.dataInicio = dataInicio;
+    }
+
+    public LocalDate getDataFim() {
+        return dataFim;
+    }
+
+    public void setDataFim(LocalDate dataFim) {
+        this.dataFim = dataFim;
+    }
+
+    public String getAnoSemestre() {
+        return anoSemestre;
+    }
+
+    public void setAnoSemestre(String anoSemestre) {
+        this.anoSemestre = anoSemestre;
+    }
+
+    public Tipos getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(Tipos tipo) {
+        this.tipo = tipo;
+    }
 }
